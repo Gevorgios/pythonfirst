7c64056e01a45cc1fdd02c6df0c5cd1a0f0a4c10
Gevorg Minasian <cool.port-965@yandex.ru>
2023-09-28 04:08:04 +0400

    first commit

---
+++ firstfile.py
@@ -0,0 +1,31 @@
+#Задание1
+x = int(input("Введите первое число: "))
+d = int(input("Введите второе число: "))
+f = int(input("Введите третье число: "))
+result = x + d + f
+print("Сумма чисел:", result)
+
+x = int(input("Введите первое число: "))
+d = int(input("Введите второе число: "))
+f = int(input("Введите третье число: "))
+result = x * d * f
+print("Произведение чисел:", result)
+
+#Задание2
+x = int(input("Введите вашу зарплату за месяц: "))
+d = int(input("Введите вашу сумму месячного платежа: "))
+f = int(input("Введите вашу задолженность за коммунальные услуги: "))
+result = x - d - f
+print("Сумма:", result)
+
+#Задание3
+x = int(input("Первая длина диагонали ромба: "))
+d = int(input("Вторая длина диагонали ромба: "))
+result = x * d /2
+print("Площадь ромба:", result)
+
+#Задание4
+print("\nTo be \nor not \nto be")
+
+#Задание5
+print("Life is what happens\n\twhen\n\tyou're busy making other plans\n\t\t\t\tJohn Lennon.")
